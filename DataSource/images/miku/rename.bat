64db2ee7e6406f34eb59e3cabe08de2d8b584ee2
dreaaim <454739958@qq.com>
2019-05-15 17:23:24 +0800

    添加miku数据集

---
+++ DataSource/images/miku/rename.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+set a = 0
+setlocal EnableDelayedExpansion
+for %%n in (*.JPG) do (
+set /A a+=1
+ren "%%n" "miku_!a!.jpg"
+)
---
+++ DataSource/spider.py
@@ -0,0 +1,35 @@
+﻿import re
+import requests
+import time
+
+i = 114
+
+def downloadPic(html, keyword):
+	global i
+	pic_url = re.findall('"objURL":"(.*?)"',html,re.S)
+	print(pic_url)
+	print('开始找'+keyword+'的图片')
+	for each in pic_url:
+		print('正在下载第'+str(i)+'张图片，图片地址：'+ str(each))
+		try:
+			pic = requests.get(each, timeout=10)
+		except Exception as e:
+			print('图片无法下载,错误原因：')
+			print(e)
+			continue
+
+		dir = 'c:/software/work/spider/images/'+ keyword + '_' + str(i) + '.jpg'
+		fp = open(dir, 'wb')
+		fp.write(pic.content)
+		fp.close()
+		i+=1
+
+if __name__ == '__main__':
+	word = input("Input key word:")
+	for epoch in range(3,20):
+		num = epoch * 60
+		url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + word   + '&pn=' + str(num)
+		result = requests.get(url)
+		downloadPic(result.text, word)
+		time.sleep(10)
+
